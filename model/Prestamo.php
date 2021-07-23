2da84a0ee250ba2fd9a51608bdc32d08d466e8d5
Einzigartig96 <75045941+Einzgartig96@users.noreply.github.com>
2021-07-23 00:12:26 -0600

    Update Model Prestamo, PrestamoEquipo, PrestamoSuministro

---
+++ model/Prestamo.php
@@ -0,0 +1,86 @@
+<!--Modelo de prestamo.-->
+<?php
+include_once "model/Connection.php";
+
+class Prestamo {
+    
+    public $idPrestamo;
+    public $idTecnico;
+    public $fechaPrestamo;
+    public $fechaEsperadaDevolucion;
+    public $cliente;
+
+
+    public function __construct($idPrestamo = 0, $idTecnico = "",$fechaPrestamo = "",$fechaEsperadaDevolucion="",
+    $cliente = "") {
+        $this->idPrestamo = $idPrestamo;
+        $this->idTecnico = $idTecnico;
+        $this->fechaPrestamo = $fechaPrestamo;
+        $this->fechaEsperadaDevolucion = $fechaEsperadaDevolucion;
+        $this->cliente = $cliente;        
+    }
+
+    /**
+     * Inserta Prestamo en DB.
+     * @return boolean
+    **/
+    function insertPrestamo() {
+        $pdo = new Connection();
+        $pdo = $pdo->open();
+        $query = "INSERT INTO prestamo (idPrestamo, idTecnico,fechaPrestamo,fechaEsperadaDevolucion,cliente) VALUES ('{$this->idPrestamo}','{$this->idTecnico}','{$this->fechaPrestamo}','{$this->fechaEsperadaDevolucion}','{$this->cliente}')";
+        $result = $pdo->prepare($query);
+        return $result->execute();
+    }
+
+    /**
+     * Retorna los prestamos disponibles en la base de datos. 
+     * Hace una lista de los mismos.
+     **/
+    function seleccionarAllPrestamo() {
+        $query = "SELECT * FROM prestamo";
+        $pdo = new Connection();
+        $pdo = $pdo->open();
+        $result = $pdo->query($query);
+        $rows = [];
+        foreach ($result->fetchAll() as $row) {
+            $rows[] = new Prestamo($row['idPrestamo'], $row['idTecnico'],$row['fechaPrestamo'],$row['fechaEsperadaDevolucion'],$row['cliente']);
+        }
+        return $rows;
+    }      
+    
+      
+    /**
+     * Retorna un prestamo disponible en la base de datos. 
+     * Hace una lista de detalles.
+     **/
+    function seleccionarPrestamo($idPrestamo = 0) {
+        $query = "SELECT * FROM prestamo";
+        $pdo = new Connection();
+        $pdo = $pdo->open();
+        $result = $pdo->query($query);
+        $rows = [];
+        foreach ($result->fetchAll() as $row) {
+            $rows[] = new Prestamo($row['idPrestamo'], $row['idTecnico'],$row['fechaPrestamo'],$row['fechaEsperadaDevolucion'],$row['cliente']);
+        }
+        return $rows;
+    }      
+    
+    /**
+     * Actualiza la información del préstamo.
+     * NO se requiere en el sistema.
+     */
+    function actualizarPrestamo(){
+        //NO se requiere en el sistema.
+    }
+    
+    /**
+     * Elimina un prestamo de la base de datos.
+     * NO se requiere en el sistema.
+     */
+    function eliminarPrestamo($idPrestamo){
+        $pdo = new Connection();
+        $queryDelete = "DELETE FROM prestamo WHERE idPrestamo = '{$idPrestamo}'";        
+        $resultDel = $pdo->open()->query($queryDelete);
+        return $resultDel->execute();
+    }
+}
---
+++ model/PrestamoSuministro.php
@@ -0,0 +1,83 @@
+<!--Modelo de prestamo de suministro.-->
+<?php
+include_once "model/Connection.php";
+
+class PrestamoSuministro {
+    
+    public $idPrestamoSuministro;
+    public $idPrestamo;
+    public $idSuministro;
+    public $idEstadoDevolucion;
+
+
+    public function __construct($idPrestamoSuministro = 0, $idPrestamo = 0,$idSuministro = 0,$idEstadoDevolucion= 0) {
+        $this->idPrestamoSuministro = $idPrestamoSuministro;
+        $this->idPrestamo = $idPrestamo;
+        $this->idSuministro = $idSuministro;
+        $this->idEstadoDevolucion = $idEstadoDevolucion;
+    }
+
+    /**
+     * Inserta Prestamo de Suministro en DB.
+     * @return boolean
+    **/
+    function insertPrestamoSuministro() {
+        $pdo = new Connection();
+        $pdo = $pdo->open();
+        $query = "INSERT INTO prestamosuministro (idPrestamoSuministro, idPrestamo,idSuministro,idEstadoDevolucion) VALUES ('{$this->idPrestamoSuministro}','{$this->idPrestamo}','{$this->idSuministro}','{$this->idEstadoDevolucion}')";
+        $result = $pdo->prepare($query);
+        return $result->execute();
+    }
+
+    /**
+     * Retorna los prestamos de suministro disponibles en la base de datos. 
+     * Hace una lista de los mismos.
+     **/
+    function seleccionarAllPrestamoSuministro() {
+        $query = "SELECT * FROM prestamosuministro";
+        $pdo = new Connection();
+        $pdo = $pdo->open();
+        $result = $pdo->query($query);
+        $rows = [];
+        foreach ($result->fetchAll() as $row) {
+            $rows[] = new PrestamoSuministro($row['idPrestamoSuministro'], $row['idPrestamo'],$row['idSuministro'],$row['idEstadoDevolucion']);
+        }
+        return $rows;
+    }      
+    
+      
+    /**
+     * Retorna un prestamo de suministro disponible en la base de datos. 
+     * Hace una lista de detalles.
+     **/
+    function seleccionarPrestamoSuministro($idPrestamoSuministro = 0) {
+        $query = "SELECT * FROM prestamosuministro";
+        $pdo = new Connection();
+        $pdo = $pdo->open();
+        $result = $pdo->query($query);
+        $rows = [];
+        foreach ($result->fetchAll() as $row) {
+            $rows[] = new PrestamoSuministro($row['idPrestamoSuministro'], $row['idPrestamo'],$row['idSuministro'],$row['idEstadoDevolucion']);
+        }
+        return $rows;
+    }      
+    
+    /**
+     * Actualiza la información del prestamo de suministro.
+     * NO se requiere en el sistema.
+     */
+    function actualizarPrestamoSuministro(){
+        //NO se requiere en el sistema.
+    }
+    
+    /**
+     * Elimina un prestamo de suministro de la base de datos.
+     * NO se requiere en el sistema.
+     */
+    function eliminarPrestamoSuministro($idPrestamoSuministro){
+        $pdo = new Connection();
+        $queryDelete = "DELETE FROM prestamosuministro WHERE idPrestamoSuministro = '{$idPrestamoSuministro}'";        
+        $resultDel = $pdo->open()->query($queryDelete);
+        return $resultDel->execute();
+    }
+}
