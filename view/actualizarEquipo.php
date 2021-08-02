53226cc79b31679e3d8d16b2f4c14f8bc338747c
Einzgartig96 <teresita.guido51@gmail.com>
2021-08-01 22:27:51 -0600

    updates

--- view/actualizarEquipo.php
+++ view/actualizarEquipo.php
@@ -17,8 +17,8 @@
             <div class="col-md-12 col-xl-12 col-sm-12" >
                 <label for="idTipoEquipo" class='label_form'>Tipo:  </label>
                 <select class="col-sm-2 col-form-label" name="idTipoEquipo" id="input_form" required>
-                <?php foreach ($allTipoEquipo as $value) { ?>
-                    <option value="<?php echo $value->idTipoEquipo; ?>"><?php echo $value->nombreTipoEquipo; ?></option>
+                <?php foreach ($allTipoEquipo as $tipo) { ?>
+                    <option value="<?php echo $tipo->idTipoEquipo; ?>"><?php echo $tipo->nombreTipoEquipo; ?></option>
                 <?php } ?>
                 </select>
             </div>
@@ -26,8 +26,8 @@
             <div class="col-md-12 col-xl-12 col-sm-12" >
                 <label for="idCondicionActual" class='label_form'>Condición actual:  </label>
                 <select class="col-sm-2 col-form-label" name="idCondicionActual" id="input_form" required>
-                <?php foreach ($allCondicionActual as $value) { ?>
-                    <option value="<?php echo $value->idCondicionActual; ?>"><?php echo $value->descripcionCondicionActual; ?></option>
+                <?php foreach ($allCondicionActual as $condicion) { ?>
+                    <option value="<?php echo $condicion->idCondicionActual; ?>"><?php echo $condicion->descripcionCondicionActual; ?></option>
                 <?php } ?>
                 </select>
             </div>
@@ -35,15 +35,15 @@
             <div class="col-md-12 col-xl-12 col-sm-12" >
                 <label for="idEstadoInventario" class='label_form'>Estado en inventario:  </label>
                 <select class="col-sm-2 col-form-label" name="idEstadoInventario" id="input_form" required>
-                <?php foreach ($allEstadoInventario as $value) { ?>
-                    <option value="<?php echo $value->idEstadoInventario; ?>"><?php echo $value->descripcionEstadoInventario; ?></option>
+                <?php foreach ($allEstadoInventario as $valor) { ?>
+                    <option value="<?php echo $valor->idEstadoInventario; ?>" selected><?php echo $valor->descripcionEstadoInventario; ?></option>    
                 <?php } ?>
                 </select>
             </div>
             <div class="dropdown-divider" id="dropdown-divider-form"></div>
             <div class="col-md-12 col-xl-12 col-sm-12" >
                 <label for="fechaInclusion" class='label_form'>Fecha de inclusión: </label>
-                <input type="date" class="col-sm-2 col-form-label" name="fechaInclusion" required>
+                <input type="date" class="col-sm-2 col-form-label" name="fechaInclusion" value="<?php echo $value->fechaInclusion; ?>" required>
             </div>            
             <div class="dropdown-divider" id="dropdown-divider-form"></div>
         </div>     
